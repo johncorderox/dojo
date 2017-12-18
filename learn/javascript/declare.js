3c3005e03e75b2056a2a14f8a1a11d0cd543a174
Amanguchi <johncorderox@gmail.com>
2017-12-18 11:38:27 -0800

    more stuff for the dojo dir

---
+++ learn/javascript/declare.js
@@ -0,0 +1,17 @@
+var num1 = "24";
+var num2 = "-17";
+var num3 = "22111";
+var num4 = "91";
+
+var string1 = "Coding Dojo";
+var string2 = "is fun";
+var string3 = "and expensive";
+
+var boolT = true;
+var boolF = false;
+
+var nothing;
+
+console.log("num1" +num1+ "num2" +num2 + "num3" +"num4"+ num4);
+console.log("String 1 " +string1 + "String2" +string2 +"String3" +string3);
+console.log("bool" +boolT, + "bool: " +boolF);
