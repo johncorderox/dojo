32d452e1b6cab1e04e3b762069f1276e1cb80884
Amanguchi <johncorderox@gmail.com>
2018-01-09 18:20:35 -0800

    python

---
+++ learn/python/dict2.py
@@ -0,0 +1,22 @@
+def dic(val):
+    count = 1
+    for first,last in val.items():
+        for value in val:
+            
+            count += 1
+
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+dic(users)
