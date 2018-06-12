c5cfa409f9c9feaa80545eaee20271974c659b87
johncorderox <johncorderox@gmail.com>
2018-06-11 17:11:45 -0700

    starting a small dashboard project

---
+++ php/dashboard/app/Http/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/find', function(){
+
+  $dojos = Dojo::all();
+  $dojos = Dojo::find(1); //gets id 1
+
+
+});
