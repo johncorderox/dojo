739a1297887e391d154ace08a3a1e94a1f55c982
johncorderox <johncorderox@gmail.com>
2018-06-18 22:28:54 -0700

    show operation now works

---
+++ php/coffee/database/migrations/2018_06_17_234023_create_coffees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoffeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coffees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('size');
+            $table->text('instructions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('coffees');
+    }
+}
