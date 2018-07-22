e05eea1fbb4c2d0aed32e28d7b56964e181f119d
johncorderox <johncorderox@gmail.com>
2018-07-22 12:39:51 -0700

    new component

---
+++ react/love/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = function () {
+
+  return "<p>Hello2</p>";
+}
+
+export default person;
--- react/love/src/App.js
+++ react/love/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<h1>React to this!</h1>
+          <Person />
       </div>
     );
   }
