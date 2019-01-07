e5f1acdca360f96b47763c6e5b285a4797b81c12
John Cordero <johncorderox@gmail.com>
2019-01-06 23:58:57 -0800

    new service

--- angular/public/src/app/app.module.ts
+++ angular/public/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpService} from './http.service';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
